699ae7943b24140914b3b90ef64c76c7698028e9
julia- <julia.ryanx@gmail.com>
2018-04-30 21:19:31 +1000

    add read, update and delete queries and mutations

--- src/index.js
+++ src/index.js
@@ -14,18 +14,35 @@ const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
     feed: () => links,
+    link: (root, args) => links.find(link => link.id === args.id)
   },
   Mutation: {
     post: (root, args) => {
       const link = {
         id: `link-${idCount++}`,
         description: args.description,
-        url: args.url,
+        url: args.url
       }
       links.push(link)
       return link
+    },
+    updateLink: (root, args) => {
+      const link = {
+        id: args.id,
+        description: args.description,
+        url: args.url
+      }
+      const index = links.findIndex(link => link.id === args.id)
+      links[index] = link
+      return link
+    },
+    deleteLink: (root, args) => {
+      const link = links.find(link => link.id === args.id)
+      const index = links.findIndex(link => link.id === args.id)
+      links.splice(index, 1)
+      return link
     }
-  },
+  }
 }
 
 const server = new GraphQLServer({
