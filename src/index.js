eac0cd289d33ae0f5148d4b1be907a721faeca6d
julia- <julia.ryanx@gmail.com>
2018-06-04 00:28:44 +1000

    add signup and login functionality to authenticate via GraphQL server

--- src/index.js
+++ src/index.js
@@ -2,55 +2,17 @@ const env = require('dotenv').config()
 
 const { GraphQLServer } = require('graphql-yoga')
 const { Prisma } = require('prisma-binding')
+
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const AuthPayload = require('./resolvers/AuthPayload')
+
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: (root, args, context, info) => {
-      return context.db.query.links({}, info)
-    },
-    link: (root, args, context, info) => {
-      const queriedLinks = context.db.query.links
-      return queriedLinks.find(link => link.id === args.id)
-    }
-  },
-  Mutation: {
-    post: (root, args, context, info) => {
-      return context.db.mutation.createLink(
-        {
-          data: {
-            url: args.url,
-            description: args.description
-          }
-        },
-        info
-      )
-    },
-    update: (root, args, context, info) => {
-      return context.db.mutation.updateLink(
-        {
-          data: {
-            url: args.url,
-            description: args.description
-          },
-          where: {
-            id: args.id
-          }
-        },
-        info
-      )
-    },
-    delete: (root, args, context, info) => {
-      return context.db.mutation.deleteLink(
-        {
-          where: {
-            id: args.id
-          }
-        },
-        info
-      )
-    }
-  }
+  Query,
+  Mutation,
+  AuthPayload
 }
+
 const db = new Prisma({
   typeDefs: 'src/generated/prisma.graphql',
   endpoint: process.env.PRISMA_ENDPOINT,
@@ -66,5 +28,4 @@ const server = new GraphQLServer({
     db
   })
 })
-
 server.start(() => console.log(`Server is running on http://localhost:4000`))
