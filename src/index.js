083d2a5333bad8cb0a6e1b3050f0838afeb9a096
julia- <julia.ryanx@gmail.com>
2018-06-03 20:29:48 +1000

    connected the server to the database

--- src/index.js
+++ src/index.js
@@ -1,53 +1,70 @@
+const env = require('dotenv').config()
+
 const { GraphQLServer } = require('graphql-yoga')
-
-let links = [
-  {
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-  }
-]
-
-let idCount = links.length
-
+const { Prisma } = require('prisma-binding')
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
-    link: (root, args) => links.find(link => link.id === args.id)
+    feed: (root, args, context, info) => {
+      return context.db.query.links({}, info)
+    },
+    link: (root, args, context, info) => {
+      const queriedLinks = context.db.query.links
+      return queriedLinks.find(link => link.id === args.id)
+    }
   },
   Mutation: {
-    post: (root, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url
-      }
-      links.push(link)
-      return link
+    post: (root, args, context, info) => {
+      return context.db.mutation.createLink(
+        {
+          data: {
+            url: args.url,
+            description: args.description
+          }
+        },
+        info
+      )
     },
-    updateLink: (root, args) => {
-      const link = {
-        id: args.id,
-        description: args.description,
-        url: args.url
-      }
-      const index = links.findIndex(link => link.id === args.id)
-      links[index] = link
-      return link
+    update: (root, args, context, info) => {
+      return context.db.mutation.updateLink(
+        {
+          data: {
+            url: args.url,
+            description: args.description
+          },
+          where: {
+            id: args.id
+          }
+        },
+        info
+      )
     },
-    deleteLink: (root, args) => {
-      const link = links.find(link => link.id === args.id)
-      const index = links.findIndex(link => link.id === args.id)
-      links.splice(index, 1)
-      return link
+    delete: (root, args, context, info) => {
+      return context.db.mutation.deleteLink(
+        {
+          where: {
+            id: args.id
+          }
+        },
+        info
+      )
     }
   }
 }
+const db = new Prisma({
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
+  debug: true
+})
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
-  resolvers
+  resolvers,
+  context: req => ({
+    ...req,
+    db
+  })
 })
 
 server.start(() => console.log(`Server is running on http://localhost:4000`))
